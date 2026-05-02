064102b8cc0aa1de8bec8d17ca8b54db9e67028a
leandoero <seventeen.iinc@gmail.com>
2026-05-02 20:33:03 +0300

    add InjectionException

---
+++ src/lab/reflection/InjectionException.java
@@ -0,0 +1,13 @@
+package lab.reflection;
+
+public class InjectionException extends RuntimeException 
+{
+    public InjectionException(String message) 
+    {
+        super(message);
+    }
+    public InjectionException(String message, Throwable cause) 
+    {
+        super(message, cause);
+    }
+}
